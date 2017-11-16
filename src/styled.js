2d0aab48bd091dfb3963d9ef86ac55ecbec28ed9
Adam Gruber <talknmime@gmail.com>
2017-11-16 11:29:25 -0500

    Add classNames to styled components for easier identification

--- src/styled.js
+++ src/styled.js
@@ -3,9 +3,12 @@ import { FlexContainer } from 'common-styled-components';
 
 export const Main = styled.main`
   flex: 1;
+  height: 100vh;
 `;
 
-export const AppInfo = styled.div`
+export const AppInfo = styled.div.attrs({
+  className: 'app-info'
+})`
   position: absolute;
   bottom: 0;
   right: 0;
@@ -13,14 +16,18 @@ export const AppInfo = styled.div`
   text-align: right;
 `;
 
-export const Score = styled.h1`
+export const Score = styled.h1.attrs({
+  className: 'score'
+})`
   font-size: 20rem;
   font-weight: normal;
   margin: 0;
   color: ${props => props.hasWon ? props.theme.marine : props.theme.matte};
 `;
 
-export const PlayerTitle = styled.h2`
+export const PlayerTitle = styled.h2.attrs({
+  className: 'player-title'
+})`
   color: #fff;
   margin: 0;
   line-height: 4rem;
@@ -38,7 +45,9 @@ export const WSConnectionIndicator = styled.p`
   color: ${props => props.connected ? props.theme.marine : props.theme.watermelon};
 `;
 
-export const DeuceNote = styled.h1`
+export const DeuceNote = styled.h1.attrs({
+  className: 'deuce-note'
+})`
   position: absolute;
   top: 50%;
   margin: 0;
@@ -49,7 +58,9 @@ export const DeuceNote = styled.h1`
   display: ${props => props.show ? 'block' : 'none'};
 `;
 
-export const Panel = styled.div`
+export const Panel = styled.div.attrs({
+  className: 'panel'
+})`
   flex-basis: 50%;
   text-align: center;
   &:first-child {
@@ -79,7 +90,9 @@ export const ServiceDot = styled.div`
   background-color: ${props => props.active ? props.theme.apricot : props.theme.plaster }
 `;
 
-export const PlayerStatus = styled.h1`
+export const PlayerStatus = styled.h1.attrs({
+  className: 'player-status'
+})`
   margin: 0 0 1.5rem 0;
   padding: 0.5rem 1rem;
   color: #fff;
