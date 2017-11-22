a9e99ae2a2f5f1a64b535342cf2962de235530b4
Adam Gruber <talknmime@gmail.com>
2017-11-22 14:52:54 -0500

    General UI improvements

--- src/styled.js
+++ src/styled.js
@@ -22,7 +22,11 @@ export const Score = styled.h1.attrs({
   font-size: 70vh;
   font-weight: normal;
   margin: 0;
-  color: ${props => props.hasWon ? props.theme.marine : props.theme.matte};
+  color: ${p => {
+    if (p.hasAdvantage) return p.theme.apricot;
+    if (p.hasWon) return p.theme.marine;
+    return p.theme.matte;
+  }}
 `;
 
 export const PlayerTitle = styled.h2.attrs({
@@ -30,8 +34,8 @@ export const PlayerTitle = styled.h2.attrs({
 })`
   color: #fff;
   margin: 0;
-  line-height: 4rem;
-  background-color: ${props => props.theme.blueberry};
+  line-height: 10vh;
+  background-color: ${p => p.theme.blueberry};
 `;
 
 export const HeaderTitle = styled.h1`
@@ -42,20 +46,22 @@ export const HeaderTitle = styled.h1`
 export const WSConnectionIndicator = styled.p`
   font-size: 0.75rem;
   margin: 0.5rem 0 0 0;
-  color: ${props => props.connected ? props.theme.marine : props.theme.watermelon};
+  color: ${p => p.connected ? p.theme.marine : p.theme.watermelon};
 `;
 
 export const DeuceNote = styled.h1.attrs({
   className: 'deuce-note'
 })`
   position: absolute;
-  top: 50%;
+  top: 10vh;
   margin: 0;
-  padding: 0.5rem 1rem;
-  line-height: 1;
+  font-size: 6vh;
+  line-height: 12vh;
   color: #fff;
-  background-color: ${props => props.theme.apricot};
-  display: ${props => props.show ? 'block' : 'none'};
+  width: 100%;
+  text-align: center;
+  background-color: ${p => p.theme.apricot};
+  display: ${p => p.show ? 'block' : 'none'};
 `;
 
 export const Panel = styled.div.attrs({
@@ -64,16 +70,15 @@ export const Panel = styled.div.attrs({
   flex-basis: 50%;
   text-align: center;
   &:first-child {
-    border-right: 1px solid ${props => props.theme.matte};
+    border-right: 1px solid ${p => p.theme.carbon};
   }
 `;
 
 export const ServeIndicatorWrap = styled(FlexContainer).attrs({
   align: 'center'
 })`
-  background-color: ${props => props.theme.matte};
-  height: 2.5rem;
   margin: 0 auto;
+  width: 100%;
 `;
 
 export const ServeIndicatorInner = styled(FlexContainer).attrs({
@@ -84,19 +89,27 @@ export const ServeIndicatorInner = styled(FlexContainer).attrs({
 `;
 
 export const ServiceDot = styled.div`
-  width: 1rem;
-  height: 1rem;
-  border-radius: 1rem;
-  background-color: ${props => props.active ? props.theme.apricot : props.theme.plaster }
+  width: 6vh;
+  height: 6vh;
+  border-radius: 6vh;
+  background-color: ${p => p.active ? p.theme.apricot : p.theme.plaster }
 `;
 
 export const PlayerStatus = styled.h1.attrs({
   className: 'player-status'
 })`
-  position: absolute;
-  margin: 0 0 1.5rem 0;
-  padding: 0.5rem 1rem;
+  font-size: 6vh;
+  line-height: 12vh;
+  margin: 0;
   color: #fff;
-  background-color: ${props => props.win ? props.theme.marine : props.theme.apricot};
-  visibility: ${props => props.visible ? 'visible' : 'hidden'};
+  background-color: ${p => p.hasWon ? p.theme.marine : p.theme.apricot};
+  visibility: ${p => p.visible ? 'visible' : 'hidden'};
+`;
+
+export const ScoreBox = styled.div.attrs({
+  className: 'score-box',
+})`
+  height: 60vh;
+  overflow: hidden;
+  position: relative;
 `;
