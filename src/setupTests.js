aa6929a8849ca7b048022efda1a459b87c01eb56
Adam Gruber <talknmime@gmail.com>
2017-11-28 11:32:22 -0500

    Test setup: add Web Speech API mocks

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,18 @@
 import { WebSocket } from 'mock-socket';
 
 window.WebSocket = WebSocket;
+
+// Mock Web Speech API
+// Note: This is incomplete and only mocks out properties/methods we use
+window.speechSynthesis = {
+  cancel: jest.fn(),
+  pause: jest.fn(),
+  paused: false,
+  pending: false,
+  speak: jest.fn(),
+  speaking: false
+};
+
+window.SpeechSynthesisUtterance = function SpeechSynthesisUtterance(sentence) {
+  this.sentence = sentence;
+};
