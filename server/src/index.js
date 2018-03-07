188d538c176099aadd907768c3f41c2b15581e48
Adam Gruber <talknmime@gmail.com>
2018-03-07 08:57:09 -0500

    server: wire up db connection and new routers

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,33 @@
+import './env';
 import koa from 'koa';
 import koaStatic from 'koa-static';
 import cors from 'kcors';
 import bodyParser from 'koa-bodyparser';
-import router from './routes';
+import respond from 'koa-respond';
+import apiRouter from './routes/api';
+import appRouter from './routes/app';
 import koaws from './middleware/ws';
+import connectToDb from './db';
 
-const port = 4000; 
+const port = process.env.SERVER_PORT || 4000;
 const app = new koa();
+
+// Mongoose
+connectToDb({
+  host: process.env.MONGO_HOST,
+  db: process.env.MONGO_DB,
+});
 
 // Middleware
 app.use(cors());
 app.use(bodyParser());
 app.use(koaStatic('./build'));
 app.use(koaws(app));
+app.use(respond());
 
 // HTTP Routes
-app.use(router.routes());
+app.use(apiRouter.routes());
+app.use(appRouter.routes());
 
 // Start the server listening
 app.listen(port, () => {
